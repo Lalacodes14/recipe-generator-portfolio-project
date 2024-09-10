cd642be94fff41caeb388d189401a0a9ee27b71e
Lalacodes14 <oratiledoriethobejane@gmail.com>
2024-09-10 17:03:17 +0200

    Added javascript interaction, added cdn for typewriter js plugin and changed css styling

--- SRC/index.js
+++ SRC/index.js
@@ -1,1 +1,9 @@
-alert("Hello World");
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.innerHTML = "Recipe will appear here";
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
