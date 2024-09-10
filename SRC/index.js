282c912284e0a018cec8e30c52d54345e65d63e7
Lalacodes14 <oratiledoriethobejane@gmail.com>
2024-09-10 18:06:45 +0200

    Added axios and API integration

--- SRC/index.js
+++ SRC/index.js
@@ -1,12 +1,22 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "Recipe will appear here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "a14e7e0d0teb73b6obfd8235a2ebc46e";
+  let prompt = `User instructions: Generate a recipe with ${instructionsInput.value}`;
+  let context =
+    "You are a cooking expert and love to write short recipes. Your mission is to generate a four line recipe in basic HTML and each sentence should be on the next line. Do not include code fencing. Make sure to follow the user instructions. Do not include a tile to the recipe. Sign the recipe with 'SheCodes AI' in <strong> element at the end of the recipe";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
